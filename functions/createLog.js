620885fbe579d0f6487eecc937c1acad6e319e59
Nitheesh S Kumar <40543713+Nitheeshskumar@users.noreply.github.com>
2022-12-24 12:38:05 +0530

    Update createLog.js

--- functions/createLog.js
+++ functions/createLog.js
@@ -4,7 +4,11 @@ exports.handler = async (event, context) => {
   const data = await getCollection();
   const body = JSON.parse(event.body);
   console.log(body)
-
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+};
   try {
    
 
@@ -12,11 +16,13 @@ exports.handler = async (event, context) => {
     return {
       statusCode: 200,
       body: 'true',
+      headers
     };
   } catch (e) {
     return {
       statusCode: 400,
       body: JSON.stringify(e),
+      headers
     };
   }
 };
